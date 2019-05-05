7a20f7efabc30150c9995832ca49573731679b67
yoyo715 <48304574+yoyo715@users.noreply.github.com>
2019-05-05 14:49:31 -0400

    test 0.0

---
+++ tests.py
@@ -0,0 +1,35 @@
+import main
+import json
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_id(self):
+        result = main.main_def()
+        t_result = json.loads(s=result)
+        ch = t_result["characterizations"]
+        self.assertEqual(ch[3]["id"], 3)
+
+
+    def test_singer(self):
+        result = main.main_def()
+        t_result = json.loads(s=result)
+        ch = t_result["characterizations"]
+        self.assertEqual(ch[3]["artist"], "Jeff Beck")
+
+    def test_name(self):
+        result = main.main_def()
+        t_result = json.loads(s=result)
+        ch = t_result["characterizations"]
+        self.assertEqual(ch[3]["title"], "All Shook Up")
+
+    def test_length(self):
+        result = main.main_def()
+        t_result = json.loads(s=result)
+        ch = t_result["characterizations"]
+        self.assertEqual(len(ch), 1001)
+
+
+if __name__ == '__main__':
+    unittest.main()
